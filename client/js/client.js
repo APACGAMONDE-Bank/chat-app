987e8bb126df2a83ef77bd4f04b74b83353854fe
Michael Mao <michael340@gmail.com>
2015-02-07 16:57:11 -0800

    Alert user on connection error and reconnect events.

--- client/js/client.js
+++ client/js/client.js
@@ -19,6 +19,9 @@ var typingTimeout;
 var ChatApp = React.createClass({
 
     getInitialState: function() {
+        client.on('connect_error', this.onConnectError);
+        client.on('reconnect', this.onReconnect);
+
         client.on('user:login:success', this.userLoginSuccess);
         client.on('user:login:failure', this.userLoginFailure);
         client.on('user:update:success', this.userUpdateSuccess);
@@ -34,7 +37,7 @@ var ChatApp = React.createClass({
             loggedIn: false,
             isTyping: false,
             usersTyping: [],
-            error: ''
+            alert: {error: false, message: ''}
         };
     },
 
@@ -42,6 +45,24 @@ var ChatApp = React.createClass({
         return {
             typingTimeoutDelay: 5000
         }
+    },
+
+    onConnectError: function() {
+        this.setState({
+            loggedIn: false,
+            isTyping: false,
+            usersTyping: [],
+            alert: {error: true, message: 'Connection to the server lost'}
+        });
+    },
+
+    onReconnect: function() {
+        this.setState({
+            self: {username: null},
+            users: [],
+            messages: [],
+            alert: {error: false, message: 'Successfully reconnected!'}
+        });
     },
 
     onLogin: function(username) {
@@ -60,7 +81,10 @@ var ChatApp = React.createClass({
 
     onLogout: function() {
         client.emit('user:logout');
-        this.setState({self: {username: null}, loggedIn: false});
+        this.setState({
+            self: {username: null},
+            loggedIn: false
+        });
     },
 
     onUserTyping: function() {
@@ -80,19 +104,26 @@ var ChatApp = React.createClass({
     },
 
     userLoginSuccess: function(username) {
-        this.setState({self: {username: username}, loggedIn: true, error: ''});
+        this.setState({
+            self: {username: username},
+            loggedIn: true,
+            alert: {error: false, message: ''}
+        });
     },
 
     userLoginFailure: function(message) {
-        this.setState({error: message});
+        this.setState({alert: {error: true, message: message}});
     },
 
     userUpdateSuccess: function(username) {
-        this.setState({self: {username: username}, error: ''});
+        this.setState({
+            self: {username: username},
+            alert: {error: false, message: ''}
+        });
     },
 
     userUpdateFailure: function(message) {
-        this.setState({error: message});
+        this.setState({alert: {error: true, message: message}});
     },
 
     updateUsers: function(users) {
@@ -114,10 +145,11 @@ var ChatApp = React.createClass({
     },
 
     render: function() {
-        var errorAlert = (
-            <div className="alert alert-danger alert-dismissible" role="alert">
+        var alertClass = this.state.alert.error ? 'alert alert-danger alert-dismissible' : 'alert alert-success alert-dismissible';
+        var alert = (
+            <div className={alertClass} role="alert">
                 <button className="close" type="button" data-dismiss="alert">&times;</button>
-                <strong>Error!</strong> {this.state.error}
+                <strong>{this.state.alert.error ? 'Error!' : ''}</strong> {this.state.alert.message}
             </div>
         );
         return (
@@ -128,7 +160,7 @@ var ChatApp = React.createClass({
                     </div>
                 </div>
                 <div className="row">
-                    {this.state.error ? errorAlert : null}
+                    {this.state.alert.message ? alert : null}
                 </div>
                 <div className="row">
                     <div className="col-md-8 col-md-push-4" id="chat-box">
